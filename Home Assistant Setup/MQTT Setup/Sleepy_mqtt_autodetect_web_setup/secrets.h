5898fdf35c751f10e9f4f1eb4f3b48bb36b1f9dc
Konstantinos Paraskevas <par.kostas@yahoo.gr>
2026-01-09 19:20:08 +0200

    Added deep sleep mqtt Arduino example

---
+++ Home Assistant Setup/MQTT Setup/Sleepy_mqtt_autodetect_web_setup/secrets.h
@@ -0,0 +1,6 @@
+// replace with your network credentials and MQTT server details
+#define SSID "Your WiFi SSID"
+#define PASSWORD "Your WiFi Password"
+#define mqtt_server "your.mqtt.server.address"
+#define MQTT_user "mqtt-user"
+#define MQTT_password "mqtt-password"
